94e20eee852b6c120eb91b5fe3699f789ecf5faf
marinnaB <botnari.marina1995@gmail.com>
2018-05-20 21:57:36 +0200

    Initial commit

---
+++ app/src/main/java/com/apprentirec/apprentirec/AccountActivity/ResetPaswordActivity.java
@@ -0,0 +1,15 @@
+package com.apprentirec.apprentirec.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apprentirec.apprentirec.R;
+
+public class ResetPaswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pasword);
+    }
+}
