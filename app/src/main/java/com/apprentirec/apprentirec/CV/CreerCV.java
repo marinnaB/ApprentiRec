ac54fb30fbc766e53b582f8df4b33fcdcab407dd
marinnaB <botnari.marina1995@gmail.com>
2018-06-15 15:35:15 +0200

    create CV activity

---
+++ app/src/main/java/com/apprentirec/apprentirec/CV/CreerCV.java
@@ -0,0 +1,14 @@
+package com.apprentirec.apprentirec.CV;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.apprentirec.apprentirec.R;
+
+public class CreerCV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creer_cv);
+    }
+}
