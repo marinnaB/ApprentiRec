9ad4492085cf0600f7f296d276c0e7160009fe99
Yannick <yannick.xuke@gmail.com>
2018-06-18 21:08:14 +0200

    Création ConsultCV Supression ModifyCV

---
+++ app/src/main/java/com/apprentirec/apprentirec/CV/ConsultCV.java
@@ -0,0 +1,15 @@
+package com.apprentirec.apprentirec.CV;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apprentirec.apprentirec.R;
+
+public class ConsultCV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consult_cv);
+    }
+}
