aabb94bb9cb4d9bc93fdb14af4a044901e74444b
Yannick <yannick.xuke@gmail.com>
2018-06-19 16:03:46 +0200

    Coder Consult CV

--- app/src/main/java/com/apprentirec/apprentirec/CV/ConsultCV.java
+++ app/src/main/java/com/apprentirec/apprentirec/CV/ConsultCV.java
@@ -5,18 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.apprentirec.apprentirec.AccountActivity.CandidateProfileActivity;
 import com.apprentirec.apprentirec.AccountActivity.LoginActivity;
+import com.apprentirec.apprentirec.AccountActivity.SignupActivity;
 import com.apprentirec.apprentirec.MainActivity;
 import com.apprentirec.apprentirec.R;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Map;
 
 public class ConsultCV extends AppCompatActivity {
 
@@ -35,6 +42,9 @@ public class ConsultCV extends AppCompatActivity {
             finish();
         }*/
 
+        auth = FirebaseAuth.getInstance();
+        store=FirebaseFirestore.getInstance();
+
         Intent intent = getIntent();
         String Msg = intent.getStringExtra(LoginActivity.CLE);
         String M = CandidateProfileActivity.EmailUser;
@@ -49,23 +59,25 @@ public class ConsultCV extends AppCompatActivity {
         t_Competence = (TextView) findViewById(R.id.edit_Competence);
         t_Langue = (TextView) findViewById(R.id.edit_Langue);
 
-        DocumentReference docRef = store.collection("Candidat").document(M).collection("CV").document();
+        DocumentReference docRef = store.collection("Candidat").document(M).collection("CV").document("CV");
 
-        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        t_Nom.setText(document.get("Nom").toString());
+        docRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        t_Nom.setText(documentSnapshot.get("Nom").toString());
+                        t_Prenom.setText(documentSnapshot.get("Prenom").toString());
+                        t_Adr.setText(documentSnapshot.get("Email").toString());
+                        t_Phone.setText(documentSnapshot.get("Tel").toString());
 
+                        t_Formation.setText(documentSnapshot.get("Formation").toString());
+                        t_Experience.setText(documentSnapshot.get("Experience").toString());
+                        t_Competence.setText(documentSnapshot.get("Competence").toString());
+                        t_Langue.setText(documentSnapshot.get("Langue").toString());
+
+                        Toast.makeText(ConsultCV.this, "ADDED", Toast.LENGTH_SHORT).show();
                     }
-                }
-            }
-        });
-
-        setContentView(t_Nom);
-
+                });
 
     }
 }
